fc752b5c42e310f548032349c892ca0990e7b74a
paranjaa <paranjaa@uregina.ca>
2023-09-14 10:16:35 -0600

    Removed extra LED config from main.c

--- lab1/main.c
+++ lab1/main.c
@@ -26,12 +26,6 @@ int main() {
 		GPIOA->CRL |=  GPIO_CRL_MODE5_0 |  GPIO_CRL_MODE5_1;
 		GPIOA->CRL &= ~GPIO_CRL_CNF5_0 &~ GPIO_CRL_CNF5_1;
 		
-		//do the same things to what's in PA0 as well to set the LED that way?
-		//prep light 0 (in PA0/A0)
-		GPIOA->CRL |=  GPIO_CRL_MODE0_0 |  GPIO_CRL_MODE0_1;
-		GPIOA->CRL &= ~GPIO_CRL_CNF0_0 &~ GPIO_CRL_CNF0_1;
-		
-
 	
 		//just infinitely blink that LED
 		while(1)
