350828bd7fa1fc45ce213425fd6568136dbbac98
paranjaa <paranjaa@uregina.ca>
2023-10-08 23:20:18 -0600

    added in a header comment and some other comments

--- lab2/main.c
+++ lab2/main.c
@@ -1,20 +1,26 @@
-
-//ENSE 452
-//Alok Paranjape
-// October 4th:
-//Lab 2: Checking if the github is working again
+/*
+ENSE 452
+Alok Paranjape
+200246370
+October 8th
+main.c calls the setup function and then all the other
+ones for input in a sequence. 
+Also has a function for toggling the LED that was good to check if
+code was actually running
+*/
 
 
 #include "stm32f10x.h"
 #include "usart.h"
 #include "CLI.h"
 
-//Connected on COM 5
+
 
 void startupCheck(void)
 {	
 	//show the program is running by toggling the onboard LED
 	GPIOA->ODR |= GPIO_ODR_ODR5;
+	//wait a second, then turn it off
 	delay();
 	GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5;
 
@@ -23,78 +29,38 @@ void startupCheck(void)
 
 int main() {
 	
+	//calling the setup function
 	serial_open();
-	startupCheck();
 	
-	//phase 1
-	//uint8_t testChar = 'a';
+	//startupCheck();
 	
+	//setting the max size of the array as 32, totally enough room
 	uint16_t testSize = 32;
 	uint8_t testCharArray[testSize];
 	
+	//used to leave the while loop, when it's zero
+	uint8_t quit_zero = 1;
 	
-	//testCharArray[0] = 't';
-	//testCharArray[1] = 'e';
-	//testCharArray[2] = 's';
-	//testCharArray[3] = 't';
-
-	
-	//uint8_t* testPointer = &testCharArray[0];
-	//while(1)
-	//{
-	//CLI_LEDON();
-	uint8_t quit1 = 1;
 	while(1)
 	{
+		//send out the initial prompt to enter a command
 		CLI_Prompt();
+		//record the user's input (and also echo it back to them so they can see)
 		CLI_Receive(testCharArray,testSize);
-		quit1 = CLI_Quit(testCharArray, testSize);
-		if(quit1 == 0)
+		//check if they put in "quit"
+		quit_zero = CLI_Quit(testCharArray, testSize);
+		if(quit_zero == 0)
 		{
-			uint8_t quit_msg[] = "Ending the program";
-			CLI_Transmit(quit_msg, (sizeof(quit_msg) / sizeof(uint8_t)));
+			//if they do, leave the loop
 			break;
 		}	
+		//after that, read the input the user put in and call the right function for it
 		CLI_Input(testCharArray, testSize);
+		//then white out the spaces so it's ready for the next run
 		CLI_Clean(testCharArray, testSize);
 	
-	}
-	
-	//}
-	
+	}	
+	//also just going to turn off the LED so it doesn't stay on while nothing's happening
+	GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5;
 	return 0;
 }
-
-		
-		//sendbyte(*testPointer);
-		//sendbyte(*(testPointer+1));
-		//sendbyte(*(testPointer+2));
-		//sendbyte(*(testPointer+3));
-		
-
-		//sendData();
-		//sendbyte(testChar);
-		//recieveData();
-		
-		//testChar = getbyte();
-		
-		//if(testChar == 'y')
-		//{
-		//	GPIOA->ODR |= GPIO_ODR_ODR5;
-		//}
-		
-		//if(testChar == 'n')
-		//{
-		//		GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5;
-		//}
-		//echoCharacter();
-		//CLI_Transmit(testPointer, testSize);
-		
-		//CLI_Receive(testCharArray, testSize);
-		//CLI_Help();
-		//GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5;
-		//CLI_Query();
-		//delay();
-		//GPIOA->ODR |= GPIO_ODR_ODR5;
-		//CLI_Query();
-		//delay();
