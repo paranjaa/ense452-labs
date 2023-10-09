91096f2a7d38eced3ef3f7b9eb37ed06f2269a38
paranjaa <paranjaa@uregina.ca>
2023-10-08 21:39:53 -0600

    added some comments to the phase 2 functions

--- lab2/CLI.h
+++ lab2/CLI.h
@@ -2,7 +2,24 @@
 #include "usart.h"
 
 //added for phase 2
+/**
+Uses sendByte in a loop a given array of values along the serial port
+so they get printed out in the terminal
+used for showing the current input, and responses to it
+and the interface in the command line interface
+@param *pData, points to an array storing values to print out
+@param Size, the size of the array, so it doesn't go out of bounds
+@pre must have already called serial_open()
+*/
 void CLI_Transmit(uint8_t *pData, uint16_t Size);
+
+/**
+Uses getByte to read in input from the user via the serial port
+so that it can get the commands to use
+@param *pData, points to an array for where to put the recieved valyes
+@param Size, the size of the array, so it doesn't go out of bounds
+@pre must have already called serial_open()
+*/
 void CLI_Receive(uint8_t *pData, uint16_t Size);
 
 //added for phase 3
