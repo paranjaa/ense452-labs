7cbcfbda75d79c8e1eb6310b63f40a23aef1bc40
paranjaa <paranjaa@uregina.ca>
2023-10-19 08:56:07 -0600

    testing to see if the new repository works

---
+++ lab4/main.c
@@ -0,0 +1,28 @@
+#include "stm32f10x.h"
+
+
+void delay()
+{
+	
+	unsigned volatile int c, d;
+   
+   for (c = 1; c <= 3000; c++)
+       for (d = 1; d <= 3000; d++)
+       {}
+}
+
+int main() {
+		RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+		//prep onboard light, in PA5?
+		GPIOA->CRL |=  GPIO_CRL_MODE5_0 |  GPIO_CRL_MODE5_1;
+		GPIOA->CRL &= ~GPIO_CRL_CNF5_0 &~ GPIO_CRL_CNF5_1;
+	
+		while(1)
+		{
+			GPIOA->ODR |= GPIO_ODR_ODR5; //turn on the onboard light
+			delay();
+			GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5; //turn off the onboard light 
+			delay();
+		}
+	
+	}
