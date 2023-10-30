ed835dad2fac3f9d922d48b49a1db18bcfe42833
paranjaa <paranjaa@uregina.ca>
2023-10-29 22:14:23 -0600

    got main to sort of work again

--- lab4/main.c
+++ lab4/main.c
@@ -20,76 +20,42 @@ int main() {
 	//calling the setup function
 	serial_open();
 	
-	//startupCheck();
+	startupCheck();
 	
 	
 	//setting the max size of the array as 8 (as shown with the max length on ledquery
 	//, didn't need 32
-	uint16_t testSize = 9;
+	uint16_t testSize = 8;
 	uint8_t testCharArray[testSize];
 	
 
-	
-	/*
-	uint8_t scroll_ANSI[] = "\x1b[5;15r";
-	CLI_Transmit(scroll_ANSI, (sizeof(scroll_ANSI) / sizeof(uint8_t)));
-	
-	uint8_t clear_ANSI[] = "\x1b[2J";
+	//clear the screen first
+	uint8_t clear_ANSI[] = "\x1b[10J";
 	CLI_Transmit(clear_ANSI, (sizeof(clear_ANSI) / sizeof(uint8_t)));
 	
-	uint8_t top_ANSI[] = "\x1b[1;1H";
+	//position the cursor so it's at the start of the screen
+	uint8_t top_ANSI[] = "\x1b[0;0H";
 	CLI_Transmit(top_ANSI, (sizeof(top_ANSI) / sizeof(uint8_t)));
 	
 	
-	uint8_t status_msg[] = "The LED is Turned OFF";
+	uint8_t status_msg[] = "LED Status: OFF";
 	CLI_Transmit(status_msg, (sizeof(status_msg) / sizeof(uint8_t)));
 	
-	uint8_t mid_ANSI[] = "\x1b[6;1H";
+	//make a scrollable window 10 lines down? Like the example
+	uint8_t mid_ANSI[] = "\x1b[10;r";
 	CLI_Transmit(mid_ANSI, (sizeof(mid_ANSI) / sizeof(uint8_t)));
-	*/
+	
 	
 
 	
-	/*
 	
-		
-	//changing the cursor to a blinking bar, mostly for the look of it
-	//not sure why it doesn't work, maybe PUTTY has specific display settings not recognized by ASCII?
-	//ESC [ 5 SP q
-	//uint8_t better_cursor_ANSI[] = "		\x1b[ 4 SP q";
-	//CLI_Transmit(better_cursor_ANSI, (sizeof(better_cursor_ANSI) / sizeof(uint8_t)));
 	
-
-	//esc = \x1b
-	//ESC[<t>;<b>r
-		
-	//set a scroll region starting at 5 and going to 25?
-	//using the example one instead
-	uint8_t scroll_ANSI[] = "\x1b[10r";
-	CLI_Transmit(scroll_ANSI, (sizeof(scroll_ANSI) / sizeof(uint8_t)));
-	
-	//erase 2 lines in display?
-	uint8_t clear_ANSI[] = "	\x1b[2J";
-	CLI_Transmit(clear_ANSI, (sizeof(clear_ANSI) / sizeof(uint8_t)));
-	
-	uint8_t top_cursor_ANSI[] = "	\x1b[1;1H";
-	CLI_Transmit(top_cursor_ANSI, (sizeof(top_cursor_ANSI) / sizeof(uint8_t)));
-	
-	//uint8_t title_msg[] = "ENSE 452 Lab 4";
-	//CLI_Transmit(title_msg, sizeof(title_msg));	
-	
-	uint8_t status_msg[] = "LED STATUS: OFF";
-	CLI_Transmit(status_msg, sizeof(status_msg));	
-	
-	uint8_t return_ANSI[] = "		\x1b[0;1H";
-	CLI_Transmit(return_ANSI, (sizeof(return_ANSI) / sizeof(uint8_t)));
-	*/
 	//used to leave the while loop, when it's zero
-	
-	
 	uint8_t quit_zero = 1;
 	
-
+	
+	
+	
 	//print the initial message
 	CLI_Prompt();
 	
