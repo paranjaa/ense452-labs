5312195bdbccf448306f4ecf92ee717f6dace444
paranjaa <paranjaa@uregina.ca>
2023-11-30 13:01:48 -0600

    commented out other code, it's just doing Blinky now

--- labProject/TestRTOS/main.c
+++ labProject/TestRTOS/main.c
@@ -11,15 +11,17 @@
 #include "task.h"
 
 #define mainBLINKY_TASK_PRIORITY 		(tskIDLE_PRIORITY + 1 )
-#define mainCLIPDISPLAY_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
-#define mainCLIPSELL_TASK_PRIORITY (tskIDLE_PRIORITY + 3)
+//#define mainCLIPDISPLAY_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
+//#define mainCLIPSELL_TASK_PRIORITY (tskIDLE_PRIORITY + 3)
+
+
 //#define mainCLI_TASK_PRIORITY (tskIDLE_PRIORITY+4)
 
 static void vBlinkTask( void * parameters);
 
-static void vPaperClipDisplayTask(void * parameters);
+//static void vPaperClipDisplayTask(void * parameters);
 
-static void vPaperClipSellTask(void * parameters);
+//static void vPaperClipSellTask(void * parameters);
 
 // static void vCLI_Task(void * parameters);
 
@@ -36,11 +38,21 @@ QueueHandle_t xCLI_Queue;
 int main(void)
 {
 	
-		
+	RCC->APB2ENR |= (1u<<2) | (1u<<4) ;
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN; //enable USART2 clock
+	GPIOA->CRL &= ~(1u<<22) &~ (1u<<23) &~(1u<<10) &~ (1u<<11);
+	GPIOA->CRL |=  (1u<<20) |  (1u<<21) | (3<<8) | (2<<10);
+	//GPIOA->CRL &= ~(1u<<22) &~ (1u<<23) &~(4<11);
+	
+	//AFIO->MAPR |= AFIO_MAPR_USART2_REMAP;
+	USART2->BRR = (8<<0) | (19<<4);  //hopefully baud 115200
+	USART2->CR1 |= USART_CR1_RE | USART_CR1_TE | USART_CR1_UE;
+	USART2->CR2 |= USART_CR2_CLKEN;	
 
 	
-	
+	xTaskCreate(vBlinkTask, "Blinky", configMINIMAL_STACK_SIZE, NULL, mainBLINKY_TASK_PRIORITY, NULL);
 	//serial_open();
+	/*
 	
 	
 	RCC->APB2ENR |= (1u<<2) | (1u<<4) ;
@@ -114,9 +126,9 @@ int main(void)
 
 
 	
-	//xTaskCreate(vBlinkTask, "Blinky", configMINIMAL_STACK_SIZE, NULL, mainBLINKY_TASK_PRIORITY, NULL);
+	//
 	
-	xTaskCreate(vPaperClipDisplayTask, "ClipDisplay", configMINIMAL_STACK_SIZE, NULL, mainCLIPDISPLAY_TASK_PRIORITY, NULL);
+	//xTaskCreate(vPaperClipDisplayTask, "ClipDisplay", configMINIMAL_STACK_SIZE, NULL, mainCLIPDISPLAY_TASK_PRIORITY, NULL);
 	
 	xTaskCreate(vPaperClipSellTask, "SellClips", configMINIMAL_STACK_SIZE, NULL, mainCLIPDISPLAY_TASK_PRIORITY, NULL);
 	
@@ -166,7 +178,7 @@ int main(void)
 	
 	
 	
-	
+	*/
 
 
 	
@@ -189,7 +201,7 @@ static void vBlinkTask( void * parameters)
 	{
 	
 		GPIOA->ODR |= (1u<<5);	
-		//USART2->DR = 0x60;
+		USART2->DR = 0x60;
 		vTaskDelay(1000);
 		
 		GPIOA->ODR &= ~(1u<<5);
@@ -205,7 +217,7 @@ static void vBlinkTask( void * parameters)
 
 
 
-
+/*
 static void vPaperClipDisplayTask( void * parameters)
 {
 	uint8_t paperclips = 0;
@@ -315,7 +327,7 @@ static void vPaperClipSellTask( void * parameters)
 
 		
 
-
+*/
 
 
 
